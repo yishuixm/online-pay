5a81007c7cb117e0932258fcb45301c3bca5213b
yishuixm <1830802211@qq.com>
2016-08-23 14:17:47 +0800

    20160823001

--- wxpay/WxPay.Config.php
+++ wxpay/WxPay.Config.php
@@ -1,10 +1,10 @@
 <?php
 class WxPayConfig
 {
-	const APPID = 'wxb76452113c71d0b1';
-	const MCHID = '1356321702';
-	const KEY = 'e10adc3949ba59abbe56e057f20f883e';
-	const APPSECRET = 'e917e9463059340391b477453d2c4dd2';
+	const APPID = '';
+	const MCHID = '';
+	const KEY = '';
+	const APPSECRET = '';
 	const SSLCERT_PATH = '../cert/apiclient_cert.pem';
 	const SSLKEY_PATH = '../cert/apiclient_key.pem';
 	const CURL_PROXY_HOST = '0.0.0.0';
